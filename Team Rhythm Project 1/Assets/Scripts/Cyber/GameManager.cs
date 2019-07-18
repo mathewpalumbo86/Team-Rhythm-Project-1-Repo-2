f3aa7c086f4533f7e02976be41bbea6ad9ac2938
Matty <mathewpalumbo86@gmail.com>
2019-07-18 16:57:34 +0800

    city explosion
    - city explosion particle plays
    - city turns off
    - updated game manager script
    - updated Beta scene
    - created city explosion material
    - updated buildings prefab

--- Team Rhythm Project 1/Assets/Scripts/Cyber/GameManager.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/GameManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // public AudioSource bgMusicSource;
+    public float bgMusicLength;
+    public float bgMusicPos;
+    public float percentageMusicComplete;
+    public float timeCheck;
+    public float endLevelThreshhold;
+
+    public GameObject city;
+    public ParticleSystem cityParticle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bgMusicLength = GameObject.FindGameObjectWithTag("MusicSource").GetComponent<AudioSource>().clip.length;
+        city = GameObject.FindGameObjectWithTag("City");
+        cityParticle = city.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bgMusicPos = Time.time;
+        percentageMusicComplete = bgMusicPos / bgMusicLength * 100;
+        // At the percentage threshhold the level ends
+        if (percentageMusicComplete >= endLevelThreshhold)
+        {
+            // Debug.Log("level ends here");
+
+            if (!cityParticle.isPlaying)
+            {
+                cityParticle.Play();
+            }
+
+            if (cityParticle.isPlaying)
+            {
+                return;
+            }
+            else
+            {
+                city.SetActive(false);
+            }
+            
+
+        }
     }
 }
