fce19a3f0f4c6e3e725e56f56f95bac58a97d4ad
L. Scott <38372089+LaserDawn84@users.noreply.github.com>
2019-07-26 14:53:55 +0800

    "Fixed" scaling with issusionary effect

--- Team Rhythm Project 1/Assets/Scripts/Cyber/City Scaling/CityScale.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/City Scaling/CityScale.cs
@@ -7,13 +7,22 @@ public class CityScale : MonoBehaviour
     [SerializeField, Tooltip("Rate at which the Object Scales")]
     float scaleRate;
 
-    
 
+    private void Start()
+    {
+        Vector3.Lerp(gameObject.transform.position, new Vector3(104.17f, gameObject.transform.position.y, gameObject.transform.position.z), 20f);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float deltaTime1 = Time.deltaTime;
-        transform.localScale += new Vector3(0f, (deltaTime1 * scaleRate), (Time.deltaTime * scaleRate)); // scales evenly
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float deltaTime1 = Time.fixedDeltaTime;
+        transform.localScale += new Vector3(0f, (deltaTime1 * scaleRate), (deltaTime1 * scaleRate)); // scales evenly
+        
     }
 }
