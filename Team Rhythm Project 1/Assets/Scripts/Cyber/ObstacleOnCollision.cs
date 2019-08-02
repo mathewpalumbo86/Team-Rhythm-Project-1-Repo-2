8af98db6cfe8462da5e135a059931a4b373ebe57
Matty <mathewpalumbo86@gmail.com>
2019-08-02 21:50:44 +0800

    Revert "Revert "updated obstacle collision""
    This reverts commit 9bceb2cf1332b35b47849ffb31bdfa5ec645e4b9.

---
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/ObstacleOnCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleOnCollision : MonoBehaviour
+{
+    // This scripts controls what happens to an obstacle when it collides with the player
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
