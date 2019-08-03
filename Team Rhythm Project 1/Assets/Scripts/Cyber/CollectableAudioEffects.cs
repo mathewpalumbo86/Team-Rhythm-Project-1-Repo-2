d1728e52e27f49fcd35c12a4edd8d56720158c72
Matty <mathewpalumbo86@gmail.com>
2019-08-03 21:34:44 +0800

    lots of terrain speed and audio changes
    - terrain speed increases based on collectables picked up now
    - boat plays speed up clip
    - added speed up particle that plays when boat speeds up
    - collectable clips play at collision location
    - created new particle mat
    - created SpeedUpParticle prefab
    - created some new tags
    - updated PreRelease scene

--- Team Rhythm Project 1/Assets/Scripts/Cyber/CollectableAudioEffects.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/CollectableAudioEffects.cs
@@ -9,12 +9,14 @@ public class CollectableAudioEffects : MonoBehaviour
     public AudioClip[] collisionSounds; // The collectables audio array
     private AudioClip collisionClip;    // The selected audio clip to play
 
-    public int index = 0; // Needs to be accessible from the CollectableOnCollision script
+    public Transform collisionPosition; // Where the audio clip should play (at the collision position (for spatial audio)). Accessed from the CollectableObCollision script
+    public int index = 0;               // Needs to be accessible from the CollectableOnCollision script
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
@@ -32,11 +34,15 @@ public class CollectableAudioEffects : MonoBehaviour
     // Plays a random clip from an array
     public void PlayCollectableEffect()
     {
+        Debug.Log("play the sound");
+
         // int index = Random.Range(0, collisionSounds.Length);
         // Plays the collectable audio clip based on which type was collided with 
         collisionClip = collisionSounds[index];
         audioSource.clip = collisionClip;
-        audioSource.PlayOneShot(collisionClip);
+        // audioSource.PlayOneShot(collisionClip);
+
+        AudioSource.PlayClipAtPoint(collisionClip, collisionPosition.position);
     }
 
    
