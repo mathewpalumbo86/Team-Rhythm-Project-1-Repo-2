e08d87975ffd7de6bc3420dbbab96b621d57ed5b
Red <43746753+RedJerome@users.noreply.github.com>
2019-08-07 16:43:13 +0800

    Changes

---
+++ Team Rhythm Project 1/Assets/Audio/Muse.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Muse : MonoBehaviour
+{
+
+
+    AudioMixerControl mixer;
+
+    [Range(-80f, 20f)]
+    public float volume;
+    [Range(-80, 20f)]
+    public float pitch;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mixer = FindObjectOfType<AudioMixerControl>();
+
+        if ( mixer = FindObjectOfType<AudioMixerControl>())
+        {
+
+
+           Debug.Log("mixer Gained");
+
+
+
+        }
+
+        
+    }
+
+    void Update()
+    {
+
+        mixer.CyberSfx1(2);
+        //mixer.CyberSfx1(volume);
+        mixer.SetMasterMusic(volume);
+        Debug.Log("Volume = " + volume);
+
+
+
+
+    }
+    //AudioMixerControl mixer;// put at start of the script calling the audio mixer script 
+    //  mixer = FindObjectOfType<AudioMixerControl>();// put in start to grab the audio mixer function and have them active 
+    //mixer.("Whatever sound effect you want to increase")// put this in update or where every the sound effect should be played 
+
+
+}
