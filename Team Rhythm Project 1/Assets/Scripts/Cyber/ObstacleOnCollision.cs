d0d289e1370fe919f7177c939b03793f361d7194
Matty <mathewpalumbo86@gmail.com>
2019-08-14 21:42:15 +0800

    changes for 1.0 build

--- Team Rhythm Project 1/Assets/Scripts/Cyber/ObstacleOnCollision.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/ObstacleOnCollision.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ObstacleOnCollision : MonoBehaviour
 {
     // This scripts controls what happens to an obstacle when it collides with the player
-    
+
+    AudioSource audioSource;
+    AudioClip audioClip;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioClip = audioSource.clip;
     }
 
     // Update is called once per frame
@@ -22,8 +26,20 @@ public class ObstacleOnCollision : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            gameObject.SetActive(false);
+            StartCoroutine(WaitForAudioToFinish());
+
         }
+    }
+
+    IEnumerator WaitForAudioToFinish()
+    {
+        audioSource.PlayOneShot(audioClip);
+
+        yield return new WaitWhile(() => audioSource.isPlaying);
+        
+        gameObject.SetActive(false);
+
+
     }
 
 }
