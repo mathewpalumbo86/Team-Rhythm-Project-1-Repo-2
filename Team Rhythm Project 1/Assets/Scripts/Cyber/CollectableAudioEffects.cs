34a76b4400ecb88f552da0f895f67d340034e77c
Matty <mathewpalumbo86@gmail.com>
2019-08-15 20:39:32 +0800

    lots of polishing changes
    - fixed boat rotation
    - added new boat mesh
    - lots of colour changes to prefabs and materials

--- Team Rhythm Project 1/Assets/Scripts/Cyber/CollectableAudioEffects.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/CollectableAudioEffects.cs
@@ -15,37 +15,33 @@ public class CollectableAudioEffects : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = gameObject.GetComponent<AudioSource>();
-        
+        audioSource = gameObject.GetComponent<AudioSource>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    int index = Random.Range(0, shoot.Length);
-        //    shootClip = shoot[index];
-        //    audioSource.clip = shootClip;
-        //    audioSource.Play();
-        //}
+        
     }
 
     // Plays a random clip from an array
     public void PlayCollectableEffect()
     {
-        // Debug.Log("play the sound");
+        StartCoroutine(WaitForAudioToFinish());
+    }
 
-        // int index = Random.Range(0, collisionSounds.Length);
+    IEnumerator WaitForAudioToFinish()
+    {
         // Plays the collectable audio clip based on which type was collided with 
         collisionClip = collisionSounds[index];
-        // audioSource.clip = collisionClip;
-        // audioSource.PlayOneShot(collisionClip);
 
         // AudioSource.PlayClipAtPoint(collisionClip, collisionPosition.position);
         audioSource.PlayOneShot(collisionClip);
 
+        yield return new WaitWhile(() => audioSource.isPlaying);
+
+        gameObject.SetActive(false);
+
     }
 
-   
 }
--- Team Rhythm Project 1/Assets/Scripts/Cyber/PlayerMovement.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/PlayerMovement.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     // Will store the controller rotation
-    public Quaternion controllerGO;     
+    public Quaternion controllerGO;    
 
     // Controller values shown in UI overlay
     public Text xText;
@@ -33,15 +33,14 @@ public class PlayerMovement : MonoBehaviour
 
     // ============================================================
 
-    // Target rotations variables to rotate the boat to when it moves
-    public Vector3 leftTargetRotation;
-    public Vector3 rightTargetRotation;
-    public Vector3 upTargetRotation;
-    public Vector3 downTargetRotation;
-    public Vector3 recenterTargetRotation;
-    
+    // Variables to rotate the boat when it moves left or right
+    public Transform leftTargetRotation;
+    public Transform rightTargetRotation;
+    public Transform upTargetRotation;
+    public Transform downTargetRotation;
+    public Transform recenterTargetRotation;
     public float rotSpeed;
-        
+
     void Start()
     {
         // grab the players (Boat) rigidbody
@@ -52,56 +51,62 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
         // calculate distance to move
-        float step = speed * Time.fixedDeltaTime; 
+        float step = speed * Time.fixedDeltaTime;
 
         // Get the controller rotation
         controllerGO = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
 
         // Converts the controller quaternion to readable values
         Vector3 controllerAngles = controllerGO.eulerAngles;
-        
+
         // Updating the text overlay
         // xText.text = "X: " + controllerAngles.x;
         // yText.text = "Y: " + controllerAngles.y; 
-        // zText.text = "Z: " + controllerAngles.z;
+        // zText.text = "Z: " + controllerAngles.z;               
 
         //=========================================================================================================
         // Checks the controller z rotation and assigns it a direction (forward, left, right). Moves the player vehicle accordingly.
         // Move the player left or right
+        if ((controllerAngles.z >= 330 && controllerAngles.z <= 360) || (controllerAngles.z >= 0 && controllerAngles.z <= 30))
+        {
+            // Vehicle doesn't move when controller points forwards.
+            ctrlHoriDirectionText.text = "Direction: Forward";
 
-        Vector3 currRotation = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            // Rotates the boat when it moves (back to centre)
+            transform.rotation = Quaternion.Lerp(transform.rotation, recenterTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
-        
+        }
+
         if ((controllerAngles.z <= 180 && controllerAngles.z >= 15))
         {
             ctrlHoriDirectionText.text = "Direction: Left";
-                        
-            // Physics based movement (-x)
-            thisRB.velocity = new Vector3(-1 * velocityModifierLR, 0, 0); // move left             
 
+            // Physics based movement (-x)
+            thisRB.velocity = new Vector3(-1 * velocityModifierLR, 0, 0); // move left
+
+            // Rotates the boat when it moves
+            transform.rotation = Quaternion.Lerp(transform.rotation, leftTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
 
         if ((controllerAngles.z >= 180 && controllerAngles.z <= 315))
         {
             ctrlHoriDirectionText.text = "Direction: Right";
-            
+
             // Physics based movement (x+)
             thisRB.velocity = new Vector3(1 * velocityModifierLR, 0, 0); // move right
-            
+
+            // Rotates the boat when it moves
+            transform.rotation = Quaternion.Lerp(transform.rotation, rightTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
 
-        if ((controllerAngles.z >= 330 && controllerAngles.z <= 360) || (controllerAngles.z >= 0 && controllerAngles.z <= 30))
-        {
-            // Vehicle doesn't move when controller points forwards.
-            ctrlHoriDirectionText.text = "Direction: Forward";
-
-            
-
-        }
-         
 
         //=========================================================================================================
         // Checks the controller X rotation and assigns it a direction (forward, left, right). Moves the player vehicle accordingly.
@@ -110,11 +115,12 @@ public class PlayerMovement : MonoBehaviour
         if ((controllerAngles.x >= 180 && controllerAngles.x <= 330))
         {
             ctrlVertDirectionText.text = "Vertical Direction: Up";
-            
+
             // Physics based movement (y-)            
             thisRB.velocity = new Vector3(0, 1 * velocityModifierUp, 0);
 
-            
+            // Rotates the boat when it moves
+            transform.rotation = Quaternion.Lerp(transform.rotation, upTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
 
@@ -125,7 +131,8 @@ public class PlayerMovement : MonoBehaviour
             // Physics based movement (y+)            
             thisRB.velocity = new Vector3(0, -1 * velocityModifierDown, 0);
 
-            
+            // Rotates the boat when it moves
+            transform.rotation = Quaternion.Lerp(transform.rotation, downTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
 
@@ -133,25 +140,14 @@ public class PlayerMovement : MonoBehaviour
         {
             // Vehicle doesn't move when controller points forwards.
             ctrlVertDirectionText.text = "Vertical Direction: Forward";
-            
 
-
+            // Rotates the boat when it moves (back to centre)
+            transform.rotation = Quaternion.Lerp(transform.rotation, recenterTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
 
-        // This combines the above rotations into one
-        // Vector3 combinedEuler;
-        // combinedEuler = new Vector3(, , );
-
-        // Converts back to quaternion and updates the rotation.
-        // transform.rotation = Quaternion.Euler(combinedEuler);
-        
-
 #if UNITY_EDITOR
         // Movement controls in the editor
-        // Debug.Log("Unity Editor");
-
-        
 
         // Use keyboard to move left (physics based movement)
         if (Input.GetKey(KeyCode.A))
@@ -159,24 +155,12 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("Move left");
 
             // Move the boat
-            thisRB.velocity = new Vector3(-1* velocityModifierLR, 0,0);
+            thisRB.velocity = new Vector3(-1 * velocityModifierLR, 0, 0);
 
-
-            //float zRotateLerpValue = Mathf.Lerp(currRotation.z, leftTargetRotation.z, rotSpeed);
-                        
-            //// Mathf.Clamp(currRotation.z, 0f , leftTargetRotation.z)
-            //if(zRotateLerpValue< leftTargetRotation.z)
-            //{
-            //    transform.Rotate(0.0f, 0.0f, zRotateLerpValue, Space.Self);
-            //}
-            
-
-            //Debug.Log(" curr: " + currRotation.z + " leftTarget: " + leftTargetRotation.z + " zRotateLerpValue: " + zRotateLerpValue);
-
+            // Rotates the boat when it moves
+            transform.rotation = Quaternion.Lerp(transform.rotation, leftTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
-        
-
 
         // Use keyboard to move left (physics based movement)
         if (Input.GetKey(KeyCode.D))
@@ -186,13 +170,8 @@ public class PlayerMovement : MonoBehaviour
             // Move the boat
             thisRB.velocity = new Vector3(1 * velocityModifierLR, 0, 0);
 
-            //float zRotateLerpValue = Mathf.Lerp(currRotation.z, rightTargetRotation.z, rotSpeed);
-
-            //// Mathf.Clamp(currRotation.z, 0f , leftTargetRotation.z)
-            //if (zRotateLerpValue < rightTargetRotation.z)
-            //{
-            //    transform.Rotate(0.0f, 0.0f, zRotateLerpValue, Space.Self);
-            //}
+            // Rotates the boat when it moves
+            transform.rotation = Quaternion.Lerp(transform.rotation, rightTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
 
@@ -203,6 +182,8 @@ public class PlayerMovement : MonoBehaviour
 
             thisRB.velocity = new Vector3(0, 1 * velocityModifierUp, 0);
 
+            // Rotates the boat when it moves
+            transform.rotation = Quaternion.Lerp(transform.rotation, upTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
 
@@ -213,13 +194,11 @@ public class PlayerMovement : MonoBehaviour
 
             thisRB.velocity = new Vector3(0, -1 * velocityModifierDown, 0);
 
-
+            // Rotates the boat when it moves
+            transform.rotation = Quaternion.Lerp(transform.rotation, downTargetRotation.rotation, Time.fixedDeltaTime * rotSpeed);
 
         }
-                
-
 #endif
-
-
     }
+
 }
